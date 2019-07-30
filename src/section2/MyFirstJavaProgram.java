11c6e23139f5f77d062bde5a32e7804a643e0f9c
League Student <league@iMac-9.attlocal.net>
2019-07-30 14:25:28 -0700

    day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,15 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
+		Robot r7 = new Robot();
+	r7.setSpeed(100);
+		r7.penDown();
+		r7.setPenColor(21,101,131);
+		for(int i=0;i<4;i++) {
+		r7.move(200);
+		r7.turn(90);
 		
+		}
 		
 	}
 }
